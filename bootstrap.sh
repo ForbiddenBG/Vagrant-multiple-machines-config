5948644df4daf9b7edcb8f671ca2cc0d93eabf26
Trendafil Rozov <132566596+ForbiddenBG@users.noreply.github.com>
2024-06-22 22:11:45 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
 apt-get update -y && apt-get upgrade -y
 apt-get install -y git
+systemctl enable git
 apt-get systemd -y
 apt-get install -y apache2
+systemctl enable apache2
 echo "* Installing Docker ..."
 apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
@@ -12,3 +14,4 @@ add-apt-repository \
 apt-get update -y
 apt-get install -y docker-ce docker-ce-cli containerd.io
 usermod -aG docker vagrant
+systemctl enable docker
